401dacc7aa052decf4611baa2494c38aa89985cb
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-05-12 01:38:19 +0300

    Refactor twitpic activity

--- app/src/main/java/co/tuzun/emrehan/twitpic/Auth.java
+++ app/src/main/java/co/tuzun/emrehan/twitpic/Auth.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.core.Callback;
--- app/src/main/java/co/tuzun/emrehan/twitpic/TweetActivity.java
+++ app/src/main/java/co/tuzun/emrehan/twitpic/TweetActivity.java
@@ -1,11 +1,14 @@
 package co.tuzun.emrehan.twitpic;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -16,6 +19,7 @@ public class TweetActivity extends ActionBarActivity {
 
     private Button tweetpicButton;
     private EditText editText;
+    private ProgressDialog generatingDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +28,24 @@ public class TweetActivity extends ActionBarActivity {
 
         editText = (EditText) findViewById(R.id.tweetpic_text);
         tweetpicButton = (Button) findViewById(R.id.tweetpic_button);
+        generatingDialog = new ProgressDialog(this);
 
         tweetpicButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),
                         "TweetPic this: " + editText.getText(),
                         Toast.LENGTH_SHORT).show();
+
+                // Create generating dialog
+                generatingDialog.setMessage("Generating TweetPic Image...");
+                generatingDialog.setIndeterminate(true);
+                generatingDialog.setCancelable(false);
+                generatingDialog.show();
+
+                // Hide keyboard
+                InputMethodManager imm = (InputMethodManager)getSystemService(
+                        getApplicationContext().INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
             }
         });
     }
