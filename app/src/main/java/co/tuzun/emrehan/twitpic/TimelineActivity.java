62ab99e3ff1ab217b8cbd8d4ccb31b4c69731c36
Emrehan Tuzun <emrehan.tuzun@gmail.com>
2015-05-12 07:09:36 +0300

    Add floating button

--- app/src/main/java/co/tuzun/emrehan/twitpic/TimelineActivity.java
+++ app/src/main/java/co/tuzun/emrehan/twitpic/TimelineActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
-import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.melnykov.fab.FloatingActionButton;
 import com.twitter.sdk.android.core.TwitterCore;
 import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
 import com.twitter.sdk.android.tweetui.UserTimeline;
@@ -18,27 +19,30 @@ public class TimelineActivity extends ListActivity {
     SwipeRefreshLayout refreshLayout;
     FloatingActionButton sendButton;
 
+    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timeline);
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), TweetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createList();
+
         refreshLayout = (SwipeRefreshLayout) findViewById(R.id.activity_main_swipe_refresh_layout);
         refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 createList();
                 refreshLayout.setRefreshing(false);
-            }
-        });
-
-        createList();
-
-        sendButton = (FloatingActionButton) findViewById(R.id.tweet_button);
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), TweetActivity.class);
-                startActivity(intent);
             }
         });
     }
@@ -49,5 +53,6 @@ public class TimelineActivity extends ListActivity {
                 .build();
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter(this, userTimeline);
         setListAdapter(adapter);
+        fab.invalidate();
     }
 }
